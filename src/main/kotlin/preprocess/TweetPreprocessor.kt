e197f3d6ba330091853ce83557680d6c2c133bbc
Serhii Fedusov <serhii.fedusov@mercedes-benz.com>
2023-03-19 21:27:42 +0100

    add links filter to the preprocessor

--- src/main/kotlin/preprocess/TweetPreprocessor.kt
+++ src/main/kotlin/preprocess/TweetPreprocessor.kt
@@ -6,15 +6,21 @@ import org.apache.spark.ml.feature.RegexTokenizer
 import org.apache.spark.ml.feature.StopWordsRemover
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.types.DataTypes
+import org.jetbrains.kotlinx.spark.api.UDF1
 import org.jetbrains.kotlinx.spark.api.col
+import scala.collection.JavaConverters
+import scala.collection.Seq
+import java.io.Serializable
 
-class TweetPreprocessor : (Dataset<Row>) -> Dataset<Row> {
+class TweetPreprocessor : (Dataset<Row>) -> Dataset<Row>, Serializable {
 
     override fun invoke(dataset: Dataset<Row>): Dataset<Row> {
         val tokenizer = RegexTokenizer()
             .setInputCol("tweet")
             .setOutputCol("tokens")
-            .setPattern("([’']s)?[,!?\\\"'`:;.—\\-“”‘’]*\\s+[,!?\\\"'`:;.—\\-“”‘’]*")
+            .setPattern("([’']s)?[,!?\"'`:;.—-“”‘’]*\\s+[,!?\"'`:;.—-“”‘’]*")
             .setMinTokenLength(2)
 
         val stopWords = StopWordsRemover().setStopWords(StopWordsRemover.loadDefaultStopWords("english"))
@@ -31,11 +37,21 @@ class TweetPreprocessor : (Dataset<Row>) -> Dataset<Row> {
 
         return dataset.filter(col("class").isNotNull)
             .run(tokenizer::transform)
-
             .run(stopWords::transform)
+            .withColumn("tokens_clean", filterLinksUDF.apply(col("tokens_clean")))
             .run(hashingTF::transform)
             .let {
                 idf.fit(it).transform(it)
             }
+            .drop("tweet", "tokens", "tokens_clean", "tf")
     }
+
+    private val filterLinksUDF = udf(
+        UDF1 { tokens: Seq<String> ->
+            JavaConverters.asJavaCollection(tokens)
+                .filterNot { it.isLink() }
+        }, DataTypes.createArrayType(DataTypes.StringType)
+    )
+
+    private fun String.isLink() = matches(Regex("^http.*"))
 }
